3b438bbac0ef63c2907e3402de068cf86b6019bb
pmiju <pmyju01@gamil.com>
2024-06-09 20:23:01 +0900

    24.06.09 front update

---
+++ src/main/java/moacloth/moashop/MoashopApplication.java
@@ -0,0 +1,13 @@
+package moacloth.moashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoashopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moacloth/moashop/MoashopApplicationTests.java
@@ -0,0 +1,13 @@
+package moacloth.moashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
